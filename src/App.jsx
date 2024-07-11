a31ad7f7fb2f7fab16a29cac14e0cca513460830
YanaLepska <lepskayana1624@gmail.com>
2024-07-11 21:46:15 +0300

    WaterMainInfo
    Add basic router for TrakerPage, components of waterMainInforConteiner and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import TrackerPage from './page/TrackerPage/TrackerPage';
 
 function App() {
-  return;
+  return (
+    <>
+      <Routes>
+        <Route path="/tracker" element={<TrackerPage />} />
+        <Route path="*" element="Not found" />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/page/TrackerPage/TrackerPage.jsx
@@ -0,0 +1,11 @@
+import WaterMainInfo from "../../components/WaterMainInfo/WaterMainInfo"
+
+const TrackerPage = () => {
+  return (
+    <div>
+      <WaterMainInfo/>
+    </div>
+  )
+}
+
+export default TrackerPage
