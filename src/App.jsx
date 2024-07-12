b7be7091ff0b35f308fcb125476d888102856a9f
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-07-12 13:32:57 +0300

    Only universal component Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
-import { useState } from 'react';
-
-import LogOutModal from './components/LogOutModal/LogOutModal.jsx';
-
 import './App.css';
 
 function App() {
-  const [logOutModalIsOpen, setLogOutModalIsOpen] = useState(false);
-
-  function openLogOutModal() {
-    setLogOutModalIsOpen(true);
-  }
-
-  function closeLogOutModal() {
-    setLogOutModalIsOpen(false);
-  }
-
-  return (
-    <>
-    <button onClick={()=>openLogOutModal()}>LogOut</button>
-    <LogOutModal 
-    logOutModalIsOpen={logOutModalIsOpen} 
-    closeLogOutModal={closeLogOutModal} />
-    </>
-    
-  )
+  return;
 }
 
 export default App;
