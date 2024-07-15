522656a6625c6fa68f12ee1f769773be92b29bb7
Yullia Kravchuk <ljuljek554@gmail.com>
2024-07-15 16:34:12 +0300

    add  redux calendar

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,18 @@
 import storage from 'redux-persist/lib/storage';
-import {FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE} from 'redux-persist'
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './auth/slice.js';
 import { timerReducer } from './timer/slice.js';
+import { calendarReducer } from './calendar/slice.js';
 
 const authPeristConfig = {
   key: 'auth',
@@ -14,8 +24,9 @@ export const store = configureStore({
   reducer: {
     auth: persistReducer(authPeristConfig, authReducer),
     countDownTimer: timerReducer,
+    calendar: calendarReducer,
   },
-  middleware: (getDefaultMiddleware) =>
+  middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
