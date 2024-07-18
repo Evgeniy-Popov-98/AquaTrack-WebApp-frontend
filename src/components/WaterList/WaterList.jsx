fd0aff643bceb0c7cb75306f202180374a4e6cf6
Evgeniy <evgeniy6183@gmail.com>
2024-07-18 05:56:26 +0300

    add_icons

--- src/components/WaterList/WaterList.jsx
+++ src/components/WaterList/WaterList.jsx
@@ -1,27 +1,70 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import css from './WaterList.module.css';
 import { selectWaterDaily } from '../../redux/water/selectors';
+
+import WaterModal from '../WaterModal/WaterModal';
+
+import css from './WaterList.module.css';
 import sprite from '../../assets/icons/icons.svg';
 
 const WaterList = () => {
   const waterDaily = useSelector(selectWaterDaily);
 
+  const [showWaterModal, setShowWaterModal] = useState(false);
+  const [operationType, setOperationType] = useState('add');
+
+  const onOpenWaterModal = type => {
+    setOperationType(type);
+    setShowWaterModal(true);
+  };
+
+  const onCloseWaterModal = () => {
+    setShowWaterModal(false);
+  };
+
   return (
-    <div className={css.waterList}>
+    <ul className={css.waterList}>
       {waterDaily.map(item => (
-        <div key={item.id} className={css.waterCard}>
-          <svg
-          //   className={clsx(css.staticPositionIcon, {
-          //     [css.activeRotateIcon]: userBarPopover,
-          //   })}
-          >
+        <li className={css.waterCard} key={item.id}>
+          <svg className={css.bottleIcon}>
             <use href={`${sprite}#icon-water-glass`} />
           </svg>
-          <p>{item.amountLiters} ml</p>
-          <p>{item.time}</p>
-        </div>
+          <div className={css.containerText}>
+            <p style={{ fontWeight: 700, fontSize: '15px' }}>
+              {item.amountLiters} ml
+            </p>
+            <p>{item.time}</p>
+          </div>
+          <div className={css.containerIcon}>
+            <button
+              className={css.button}
+              onClick={() => onOpenWaterModal('edit')}
+              type="button"
+            >
+              <svg className={css.setingsIcon}>
+                <use href={`${sprite}#icon-edit-2`} />
+              </svg>
+            </button>
+            <button
+              className={css.button}
+              onClick={() => onOpenWaterModal('delete')}
+              type="button"
+            >
+              <svg className={css.setingsIcon}>
+                <use href={`${sprite}#icon-trash-04`} />
+              </svg>
+            </button>
+          </div>
+        </li>
       ))}
-    </div>
+      {showWaterModal && (
+        <WaterModal
+          waterModalOpen={showWaterModal}
+          closeWaterModal={onCloseWaterModal}
+          operationType={operationType}
+        />
+      )}
+    </ul>
   );
 };
 
--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -1,13 +1,21 @@
-import Modal from "../Modal/Modal.jsx"
-import WaterForm from "../WaterForm/WaterForm.jsx"
-import css from './WaterModal.module.css'
+import Modal from '../Modal/Modal.jsx';
+import WaterForm from '../WaterForm/WaterForm.jsx';
+import css from './WaterModal.module.css';
 
 const WaterModal = ({ waterModalOpen, closeWaterModal, operationType }) => {
   if (!waterModalOpen) return null;
-  
-  const title = operationType === "add" ? "Add Water" : "Edit the entered amount of water";
-  const value = operationType === "add" ? "Choose a value:" : "Correct entered data:";
-  return (
+
+  let swithModalDelete = false;
+
+  const title =
+    operationType === 'add' ? 'Add Water' : 'Edit the entered amount of water';
+  const value =
+    operationType === 'add' ? 'Choose a value:' : 'Correct entered data:';
+
+  if (operationType === 'delete') {
+    swithModalDelete = true;
+  }
+  return swithModalDelete ? (
     <Modal modalIsOpen={waterModalOpen} closeModal={closeWaterModal}>
       <div className={css.waterModal}>
         <p className={css.modalTitle}>{title}</p>
@@ -15,7 +23,15 @@ const WaterModal = ({ waterModalOpen, closeWaterModal, operationType }) => {
         <WaterForm closeWaterModal={closeWaterModal} />
       </div>
     </Modal>
-  )
-}
+  ) : (
+    <Modal modalIsOpen={waterModalOpen} closeModal={closeWaterModal}>
+      <div className={css.waterModal}>
+        <p className={css.modalTitle}>{title}</p>
+        <p className={css.modalValue}>{value}</p>
+        <WaterForm closeWaterModal={closeWaterModal} />
+      </div>
+    </Modal>
+  );
+};
 
-export default WaterModal
+export default WaterModal;
