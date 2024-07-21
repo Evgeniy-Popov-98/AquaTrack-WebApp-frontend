0d2ded35341b053924483a02fed2cc1d27aa127b
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-07-21 17:49:15 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Зміна порту на 3000
+    port: 4000, // Зміна порту на 4000
   },
 });
