019f144c8b664c79d8dbc30170206bdefc624353
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-07-23 12:27:52 +0300

    Update store.js
    Додав збереження в локалсторейдж стану чи залогінений користувач. Це прибрало проблему з оновленням сторінки і викиданням на сторінку логіну

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,7 +19,7 @@ import { refreshSettingInterceptors } from './auth/operations.js';
 const authPeristConfig = {
   key: 'auth',
   storage,
-  whitelist: ['accessToken'],
+  whitelist: ['accessToken', 'isLoggedIn'],
 };
 
 export const store = configureStore({
